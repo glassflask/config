5532cc37f117005a062053a623eb66b9e2e247d6
glassflask <lukasringle@gmail.com>
2017-03-22 17:33:52 +0100

    Add files via upload

---
+++ mousekeyboard.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+cat <<EOT >> /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
+"Section "InputClass"\n
+	Identifier "My Mouse"\n
+	MatchIsPointer "yes"\n
+	Option "AccelerationProfile" "-1"\n
+	Option "AccelerationScheme" "none"\n
+	Option "AccelSpeed" "-1"\n
+EndSection
+
+Section "InputClass"
+	Identifier "My Mouse"
+	MatchIsPointer "yes"
+	Option "AccelerationProfile" "-1"
+	Option "AccelerationScheme" "none"
+	Option "AccelSpeed" "-1"
+EndSection"
+EOT
+
+##keyboard
+
+localectl set-keymap de-latin1-nodeadkeys
+
+localectl --no-convert set-x11-keymap de pc105 
+nodeadkeys
